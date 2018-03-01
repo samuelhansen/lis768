db07d5dfeac8daaedbce882f1d951f8c564c5155
samuelhansen <github@acmescience.com>
2018-02-28 18:51:06 -0600

    ocr

--- assignment2/commandLineOCR.py
+++ assignment2/commandLineOCR.py
@@ -1,15 +1,40 @@
+import sys
 import pytesseract
 from wand.image import Image
 from PIL import Image as PI
 import os
 import io
 
+helpString = "ocr.py -- ocr a directory of pdfs with tesseract!\nUseage: ./ocr.py  [Path to PDF Directory] [Path to Output Directory]"
+
 path=os.path.dirname(os.path.abspath(__file__))
 
 
-
-ocrPath=path + '/ocredTexts/'
+ocrPath = path + '/ocredTexts/'
 toOcrPath = path + '/filesToTranscribe/'
+
+# check for invalid or help options
+if len(sys.argv)>1:
+    if len(sys.argv) > 3:
+        print("invalid options")
+        print(helpString)
+        quit()
+
+    if sys.argv[1] == "-h" or sys.argv[1] == "help":
+        print(helpString)
+        quit()
+
+    if len(sys.argv) >= 2:
+        if sys.argv[1].endswith('/'):
+            toOcrPath = sys.argv[1]
+        else:
+            toOcrPath = sys.argv[1] + '/'
+
+    if len(sys.argv) == 3:
+        if sys.argv[2].endswith('/'):
+            ocrPath = sys.argv[2]
+        else:
+            ocrPath = sys.argv[2] + '/'
 
 
 
@@ -42,7 +67,4 @@ for fName in sorted(os.listdir(toOcrPath)):
                 file.write(x + '\n')
 
 
-
-
-
-
+print(ocrPath + ' ' + toOcrPath)
