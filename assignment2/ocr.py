2e5b567959caed8903c62d1086de386074cee201
samuelhansen <github@acmescience.com>
2018-02-28 18:26:57 -0600

    ocr

--- assignment2/ocr.py
+++ assignment2/ocr.py
@@ -5,38 +5,39 @@ import os
 import io
 
 path=os.path.dirname(os.path.abspath(__file__))
+ocrPath=path + '/ocredTexts/'
+toOcrPath = path + '/filesToTranscribe/'
 
-path +='/1927-0013-Wall.pdf'
-# # path='asdf.txt'
-# #
+if not os.path.exists(ocrPath):
+    os.makedirs(ocrPath)
+
 pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
-req_image = []
-final_text = []
-
-image_pdf = Image(filename=path, resolution=600)
-image_jpeg = image_pdf.convert('jpeg')
-
-for img in image_jpeg.sequence:
-    img_page = Image(image=img)
-    req_image.append(img_page.make_blob('jpeg'))
 
 
-for img in req_image:
-    txt = pytesseract.image_to_string(PI.open(io.BytesIO(img)))
-    final_text.append(txt)
+for fName in sorted(os.listdir(toOcrPath)):
+    if fName.endswith('.pdf'):
+        req_image = []
+        final_text = []
+        noExtFName=os.path.splitext(fName)[0]
+        pdfPath=toOcrPath + fName
+        image_pdf = Image(filename=pdfPath, resolution=300)
+        image_jpeg = image_pdf.convert('jpeg')
 
-with open('ocrPDF.txt', 'a') as file:
-    for x in final_text:
-        file.write(x + '\n')
+        for img in image_jpeg.sequence:
+            img_page = Image(image=img)
+            req_image.append(img_page.make_blob('jpeg'))
+
+
+        for img in req_image:
+            txt = pytesseract.image_to_string(PI.open(io.BytesIO(img)))
+            final_text.append(txt)
+        ocrText=ocrPath + noExtFName + '.txt'
+        with open(ocrText, 'a') as file:
+            for x in final_text:
+                file.write(x + '\n')
 
 
 
-#now lets convert the PDF to Image
-    #this is good resolution As far as I know
-# with Image(filename=path, resolution=200) as img:
-#     #keep good quality
-#     img.compression_quality = 80
-#     #save it to tmp name
-#     img.save(filename="temp/temp%s.jpg" % uuid_set)
+
 
 
